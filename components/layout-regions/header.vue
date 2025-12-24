d8bfff9df489f8c669681578da10a71e5f462d6e
Igor Deych <hurgadan@gmail.com>
2025-12-24 18:10:00 +0200

    feat: git initial

---
+++ components/layout-regions/header.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <nav class="p-6">
+    <div class="max-w-7xl mx-auto flex justify-between items-center">
+      <div class="flex items-center gap-2">
+        <div class="bg-white p-2 rounded-xl shadow-lg rotate-3">
+          <span class="text-2xl">🔗</span>
+        </div>
+        <span class="text-2xl font-bold text-white drop-shadow-md tracking-wide">
+            Shorty
+          </span>
+      </div>
+      <div class="hidden md:flex gap-4">
+        <ui-button variant="ghost" size="sm">
+          Log In
+        </ui-button>
+        <ui-Button variant="secondary" size="sm" class="shadow-none border-0">
+          Sign Up Free
+        </ui-Button>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div
+      class="min-h-screen w-full bg-gradient-to-br from-pink-400 via-purple-500 to-blue-500 animate-gradient flex flex-col">
+    <layout-regions-header/>
+    <slot/>
+    <layout-regions-footer/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
